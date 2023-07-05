29b106e615f618dc426fbf6698cc9879670cdc5a
Tino B <bomantino@gmail.com>
2023-07-05 10:53:34 +0300

    consumer base logic done

---
+++ kafka/consumer.py
@@ -0,0 +1,29 @@
+import json
+import sys
+from cassandra.cluster import Cluster
+from kafka import KafkaConsumer
+
+CASSANDRA_PORT = 9042
+CASSANDRA_KEYSPACE = "kafka_datastream"
+CASSANDRA_TABLE = "datastream_table"
+KAFKA_HOST = "localhost:9092"
+KAFKA_TOPIC = "kafka-datastream"
+
+try:
+    # kafka connection
+    kafka_consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers = KAFKA_HOST,
+                            value_deserializer = lambda v: json.loads(v.decode("utf-8")))
+    # cassandra connection
+    cassandra_cluster = Cluster(port = CASSANDRA_PORT)
+    cassandra_session = cassandra_cluster.connect(keyspace = CASSANDRA_KEYSPACE)
+    
+except Exception as e:
+    print(f"Error --> {e}")
+    sys.exit()
+
+for message in kafka_consumer:
+    sensor_id = message.key.decode("utf-8")
+    message_values = message.value
+    timestamp, temperature, rh = message_values["timestamp"], message_values["temperature"], message_values["rh"]
+    
+    # TODO logic to add values to the cassandra table
